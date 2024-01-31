be3dbb88f1f968805942760013b86af319401dc4
DevAdedeji <adedejitewogbade2@gmail.com>
2024-01-31 21:33:11 +0100

    finished setting up project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1 class="text-[red]">Hello</h1>
+  </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#151619',
+        black2: '#1D1F22',
+        lightBlack: '#2B2D31',
+        veryLightBlack: '#35393F',
+        darkGrey: '#5A6069',
+        grey: '#7C8187',
+        lightGrey: 'C1C4CB',
+        veryLightGrey: '#E4E4E4',
+        greyish: '#F5F5F5',
+        white: '#FFFFFF',
+        orange: '#E46643',
+        lightOrange: '#F39765'
+      }
+    }
+  },
+  plugins: []
+}
