b7901955d2acf7138525f8637663895df342c4f8
DevAdedeji <adedejitewogbade2@gmail.com>
2024-02-14 22:29:34 +0100

    Installed necessary packages

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,20 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import InlineSvg from 'vue-inline-svg';
+import VueMeta from 'vue-meta'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
-import InlineSvg from 'vue-inline-svg';
+
 
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
+app.use(VueMeta)
 app.component('inline-svg', InlineSvg);
 app.mount('#app')
